54de3ce681ee72c903da7288c409ff366b276938
baeksoo <kyubeom03@naver.com>
2022-10-08 19:22:02 +0900

    feat: Random값이 4이상이면 전진하고, 3이하면 멈추어야 한다

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,10 +1,14 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
     private final CarName name;
+    private final CarOffset offset;
 
-    private RacingCar(CarName name) {
+    private RacingCar(CarName name, CarOffset offset) {
         this.name = name;
+        this.offset = offset;
     }
 
     public CarName getName() {
@@ -12,6 +16,20 @@ public class RacingCar {
     }
 
     public static RacingCar of(String name) {
-        return new RacingCar(CarName.of(name));
+        return new RacingCar(CarName.of(name), new CarOffset());
+    }
+
+    public void move() {
+        if (canMove()) {
+            offset.increaseOffset();
+        }
+    }
+
+    public Integer getOffset() {
+        return offset.getOffset();
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
