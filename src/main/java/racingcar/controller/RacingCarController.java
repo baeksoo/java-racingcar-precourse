4f4b53b15ab5e896b625385ee52e65530fddd15a
baeksoo <kyubeom03@naver.com>
2022-10-08 18:09:37 +0900

    feat: 사용자는 자동차 이동 횟수를 입력해야 한다

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.Cars;
+import racingcar.domain.RacingRound;
 import racingcar.view.RacingView;
 
 public class RacingCarController {
@@ -17,6 +18,9 @@ public class RacingCarController {
         racingView.printInputCarNames();
         List<String> names = getNamesByUserInput();
         Cars cars = Cars.of(names);
+        racingView.printInputNumberOfAttempts();
+        String round = Console.readLine();
+        RacingRound racingRound = RacingRound.of(round);
     }
 
     private List<String> getNamesByUserInput() {
